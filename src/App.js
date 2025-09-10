29e65a3b662e83b2d7118dfe3c658e2ff2898a33
Temoor Hussain <temoorh48@gmail.com>
2025-09-10 18:43:06 +0500

    Fix: language JSON fetch for GitHub Pages + Little Lemon logo path issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import $ from "jquery";
 import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -10,103 +9,62 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import ScrollToTopFile from "./components/ScrollToTopFile";
 
-
-
-
 class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      foo: "bar",
       resumeData: {},
       sharedData: {},
     };
   }
 
-  applyPickedLanguage(pickedLanguage, oppositeLangIconId) {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+  componentDidMount() {
+    // Default language: English
+    this.applyPickedLanguage(window.$primaryLanguage);
+  }
+
+  applyPickedLanguage = (pickedLanguage) => {
     document.documentElement.lang = pickedLanguage;
 
-    // Resume data (English/French)
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
+    // Set resume JSON path
+    const resumePath =
+      pickedLanguage === window.$primaryLanguage
+        ? process.env.PUBLIC_URL + "/res_primaryLanguage.json"
+        : process.env.PUBLIC_URL + "/res_secondaryLanguage.json";
 
-    // Shared portfolio data (English/French)
-    var sharedPath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `portfolio_shared_data.json`
-        : `portfolio_shared_data2.json`;
-    this.loadSharedData(sharedPath);
-  }
+    // Set shared portfolio JSON path
+    const sharedPath =
+      pickedLanguage === window.$primaryLanguage
+        ? process.env.PUBLIC_URL + "/portfolio_shared_data.json"
+        : process.env.PUBLIC_URL + "/portfolio_shared_data2.json";
 
-  swapCurrentlyActiveLanguage(oppositeLangIconId) {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
+    this.loadJSON(resumePath, "resumeData");
+    this.loadJSON(sharedPath, "sharedData");
+  };
 
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
-  }
-
-  componentDidMount() {
-    this.loadSharedData(`portfolio_shared_data.json`); // default English
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
-  }
-
-  loadResumeFromPath(path) {
-    $.ajax({
-      url: path,
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data });
-      }.bind(this),
-      error: function (xhr, status, err) {
-        alert(err);
-      },
-    });
-  }
-
-  loadSharedData(path = `portfolio_shared_data.json`) {
-    $.ajax({
-      url: path,
-      dataType: "json",
-      cache: false,
-      success: function (data) {
-        this.setState({ sharedData: data });
-        document.title = `${data.basic_info.name}`;
-      }.bind(this),
-      error: function (xhr, status, err) {
-        alert(err);
-      },
-    });
-  }
+  loadJSON = (path, stateKey) => {
+    fetch(path)
+      .then((res) => {
+        if (!res.ok) throw new Error(`Failed to load ${path}`);
+        return res.json();
+      })
+      .then((data) => this.setState({ [stateKey]: data }))
+      .catch((err) => console.error(err));
+  };
 
   render() {
+    const { resumeData, sharedData } = this.state;
+
     return (
       <div>
-        <Header sharedData={this.state.sharedData.basic_info} />
+        {/* Header */}
+        <Header sharedData={sharedData.basic_info} />
 
         {/* Language Switch */}
         <div className="col-md-12 mx-auto text-center language">
+          {/* English */}
           <div
-            onClick={() =>
-              this.applyPickedLanguage(
-                window.$primaryLanguage,
-                window.$secondaryLanguageIconId
-              )
-            }
+            onClick={() => this.applyPickedLanguage(window.$primaryLanguage)}
             style={{ display: "inline" }}
           >
             <span
@@ -117,13 +75,9 @@ class App extends Component {
             ></span>
           </div>
 
+          {/* French */}
           <div
-            onClick={() =>
-              this.applyPickedLanguage(
-                window.$secondaryLanguage,
-                window.$primaryLanguageIconId
-              )
-            }
+            onClick={() => this.applyPickedLanguage(window.$secondaryLanguage)}
             style={{ display: "inline" }}
           >
             <span
@@ -137,32 +91,24 @@ class App extends Component {
 
         {/* Sections */}
         <About
-          resumeBasicInfo={this.state.resumeData.basic_info}
-          sharedBasicInfo={this.state.sharedData.basic_info}
+          resumeBasicInfo={resumeData.basic_info}
+          sharedBasicInfo={sharedData.basic_info}
         />
         <Projects
-          resumeProjects={this.state.resumeData.projects}
-          resumeBasicInfo={this.state.resumeData.basic_info}
+          resumeProjects={resumeData.projects}
+          resumeBasicInfo={resumeData.basic_info}
         />
         <Skills
-          sharedSkills={this.state.sharedData.skills}
-          resumeBasicInfo={this.state.resumeData.basic_info}
+          sharedSkills={sharedData.skills}
+          resumeBasicInfo={resumeData.basic_info}
         />
         <Experience
-          resumeExperience={this.state.resumeData.experience}
-          resumeBasicInfo={this.state.resumeData.basic_info}
+          resumeExperience={resumeData.experience}
+          resumeBasicInfo={resumeData.basic_info}
         />
-        <Education
-         education={this.state.resumeData.education} />
-
-        <Footer 
-        sharedBasicInfo={this.state.sharedData.basic_info} />
-
-        <ScrollToTopFile
-        sharedBasicInfo={this.state.sharedData.basic_info} />
-
-      
-
+        <Education education={resumeData.education} />
+        <Footer sharedBasicInfo={sharedData.basic_info} />
+        <ScrollToTopFile sharedBasicInfo={sharedData.basic_info} />
       </div>
     );
   }
